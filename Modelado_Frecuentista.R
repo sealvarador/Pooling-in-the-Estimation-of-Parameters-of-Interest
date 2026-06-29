d42f397f49e4f63192647f4db77838e98fca160d
sealvarador <sealvarador@unal.edu.co>
2026-06-29 00:59:04 -0500

    Modelado_Frecuentista.R

--- Modelado_Frecuentista.R
+++ Modelado_Frecuentista.R
@@ -33,11 +33,11 @@ df_areas <- read.csv("complete_birth_rec.csv", stringsAsFactors = FALSE)        
 
 df_areas$type_area <- factor(
   df_areas$type_area,
-  levels = c("urbana", "rural")
+  levels = c("rural", "urbana")
 )
 df_areas$high_edu <- factor(
   df_areas$high_edu,
-  levels = c("superior", "secundaria", "primaria", "sin educación")
+  levels = c("sin educación", "primaria", "secundaria", "superior")
 )
 df_areas$ethnic_rec <- factor(
   df_areas$ethnic_rec,
